4f110a3d97a9429709ec5c929728b170cc592164
goonan24 <jgoonan24@gmail.com>
2023-10-21 12:22:10 -0400

    commit

--- src/personal-project.js
+++ src/personal-project.js
@@ -40,7 +40,7 @@ static styles = css`
       position: absolute;
       color: green;
       left: 200px;
-      top: 62px;   
+      top: 60px;   
       right: 200px;
       font-size: 15px;
       
@@ -49,14 +49,16 @@ static styles = css`
       position: absolute;
       color: green;
       top: 115px;   
-      right: 50px;
+      right: 200px;
+      left: 200px;
       font-size: 15px;
     }
     div.weight-class{
       position: absolute;
       color: green;
       top: 170px;   
-      right: 60px;
+      right: 200px;
+      left: 200px;
       font-size: 15px;
 
     }
@@ -64,7 +66,8 @@ static styles = css`
       position: absolute;
       color: green;
       top: 220px;   
-      right: 110px;
+      right: 90px;
+      left: 200px;
       font-size: 15px;
       
     }
@@ -72,7 +75,8 @@ static styles = css`
       position: absolute;
       color: green;
       top: 270px;   
-      right: 110px;
+      right: 90px;
+      left: 200px;
       font-size: 15px;
 
     }
@@ -80,28 +84,32 @@ static styles = css`
       position: absolute;
       color: green;
       top: 320px;   
-      right: 100px;
+      right: 85px;
+      left: 200px;
       font-size: 15px;
     }
     div.Total{
       position: absolute;
       color: green;
       top: 380px;   
-      right:100px;
-      font-size: 15px;
-    }
-    div.Dis-Total{
-      position: absolute;
-      color: green;
-      top: 420px;   
-      right:-20px;
+      right: 20px;
+      left: 200px;
       font-size: 15px;
     }
     div.total-needed{
       position: absolute;
       color: green;
-      top: 460px;   
-      right:-20px;
+      top: 430px;   
+      right: 10px;
+      left: 200px;
+      font-size: 15px;
+    }
+    div.Dis-Total{
+      position: absolute;
+      color: green;
+      top: 475px;   
+      right: -220px;
+      left: 150px;
       font-size: 15px;
     }
     
@@ -113,6 +121,7 @@ static styles = css`
     this.squatValue = 0;
     this.benchValue = 0;
     this.deadliftValue = 0;
+    this.gender = "";
   }
 
   calculate() {
@@ -125,6 +134,44 @@ static styles = css`
       this.shadowRoot.querySelector('#display-total').value = sum;
     } else {
       alert('Please enter valid numbers for squat, bench, and deadlift.');
+    }
+  }
+
+  disFromTot(){
+    /*
+    const squat = parseInt(this.querySelector('#squat').value);
+    const bench = parseInt(this.querySelector('#bench').value);
+    const deadlift = parseInt(this.querySelector('#dl').value);
+    */
+   /*
+    const squat = parseInt(this.squatValue);
+    const bench = parseInt(this.benchValue);
+    const deadlift = parseInt(this.deadliftValue);
+    const total = squat + bench + deadlift;
+    const gender = parseFloat(this.gender);
+    let totneeded = 0;
+    /*
+    if(this.querySelector('#gender').textContent==="Male"){
+      totneeded = 485;
+      const disfromtot = totneeded-total;
+    
+    if (gender === "male") {
+      totneeded = 485;
+      const disfromtot = totneeded - total;
+      this.shadowRoot.querySelector('#Dis-total').value = disfromtot;
+    }
+    */
+    const squat = parseInt(this.querySelector('#squat').value);
+    const bench = parseInt(this.querySelector('#bench').value);
+    const deadlift = parseInt(this.querySelector('#dl').value);
+    const total = squat + bench + deadlift;
+    const gender = this.querySelector('#gender').textContent.toLowerCase(); // Make it lowercase for case-insensitive comparison
+    let totneeded = 0;
+
+    if (gender === 'male') {
+        totneeded = 485; // You might want to adjust this for different genders
+        const disfromtot = totneeded - total;
+        this.shadowRoot.querySelector('#dis-total').value = disfromtot;
     }
   }
 
@@ -144,7 +191,7 @@ static styles = css`
           <textarea class="distance-from-total">Distance from Total</textarea>
         </div>
 
-        <div class="unit">
+        <div class="unit" id="unit">
           <!--form class="unit" action="/url" method="get">
             <label  for="Unit"> KG OR LBS:</label>
                 <input type="text" name="username" minlength="1" maxlength="20" required>
@@ -156,7 +203,7 @@ static styles = css`
             <option value="Lbs">LBS</option>
           </select>
         </div>
-        <div class="gender">
+        <div class="gender" id="gender">
           <!--form class="Gender" action="/url" method="get">
             <label  for="Gender"> Male, Female, or RX</label>
                 <input type="text" name="gender" minlength="1" maxlength="5" required>
@@ -169,7 +216,7 @@ static styles = css`
             <option value="RX">RX</option>
           </select>
         </div>
-        <div class="weight-class">
+        <div class="weight-class" id="weight-class">
           <!--form class="weight-class" action="/url" method="get">
             <label  for="weight-class"> Weight Class is KG</label>
                 <input type="number" name="weight-class" minlength="1" maxlength="5" required>
@@ -224,11 +271,12 @@ static styles = css`
         <div class="total-needed">
           <label for="total-needed">Total Needed </label>
           <input type="number" name="total-needed" id="display-total">
+          <button @click="${this.find}" type="button">Calculate</button>
         </div>
-
         <div class="Dis-Total">
           <label for="Dis-Total"> How far above or below the total you are </label>
-          <input type="number" name="display-total">
+          <input type="number" name="dis-total" id="Dis-total">
+          <button @click="${this.disFromTot}" type="button">Calculate</button>
         </div>
       </main>
 
